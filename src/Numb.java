de4dc8b00a6b7c7328a4f7d76cf3e984c5c66474
mbabak <muskababak@ymail.com>
2020-06-17 16:43:12 -0400

    num

--- src/Numb.java
+++ src/Numb.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public interface Numb {
+public class Numb {
     public static void main(String[] args){
         Scanner userInput=new Scanner (System.in);
         System.out.println("Enter a number: ");
